89e7045120c59e2a22cda0fd8d03acd978a6f197
Jinbo <jinboom@foxmail.com>
2025-06-27 06:32:19 +0000

    style: format all code

--- include/dlFunction.h
+++ include/dlFunction.h
@@ -1,12 +1,12 @@
-#ifndef __DLZERO_FUNCTION_H__
-#define __DLZERO_FUNCTION_H__
+#ifndef __ORIGIN_DL_FUNCTION_H__
+#define __ORIGIN_DL_FUNCTION_H__
 
 #include "dlVariable.h"
 
 namespace dl
 {
 
-class Function: public std::enable_shared_from_this<Function>
+class Function : public std::enable_shared_from_this<Function>
 {
   public:
     virtual ~Function() {}
@@ -15,13 +15,12 @@ class Function: public std::enable_shared_from_this<Function>
 
     virtual NdArray Forward(const NdArray &x) = 0;
 
-	  virtual NdArray Backward(const NdArray& gy) = 0;
+    virtual NdArray Backward(const NdArray &gy) = 0;
 
-	public:
-		VariablePtr input; // 前向传播的入参
+  public:
+    VariablePtr input;  // 前向传播的入参
 
-    VariablePtr output; // 前向传播的输出
-
+    VariablePtr output;  // 前向传播的输出
 };
 
 class Square : public Function
@@ -29,7 +28,7 @@ class Square : public Function
   public:
     NdArray Forward(const NdArray &x) override;
 
-	NdArray Backward(const NdArray& gy) override;
+    NdArray Backward(const NdArray &gy) override;
 };
 
 class Exp : public Function
@@ -37,7 +36,7 @@ class Exp : public Function
   public:
     NdArray Forward(const NdArray &x) override;
 
-	NdArray Backward(const NdArray& gy) override;
+    NdArray Backward(const NdArray &gy) override;
 };
 
 extern NdArray NumericalDiff(std::function<Variable(Variable)> f, const Variable &x, data_t eps = 1e-4);
--- tests/originDL/backward/main.cpp
+++ tests/originDL/backward/main.cpp
@@ -17,23 +17,23 @@ int main()
     // b.grad = std::make_shared<NdArray>(C.Backward(*y.grad));
     // a.grad = std::make_shared<NdArray>(B.Backward(*b.grad));
     // x.grad = std::make_shared<NdArray>(A.Backward(*a.grad));
-    // print(*x.grad);   
+    // print(*x.grad);
 
     auto A = FunctionPtr(new Square());
-	auto B = FunctionPtr(new Exp());
-	auto C = FunctionPtr(new Square());
+    auto B = FunctionPtr(new Exp());
+    auto C = FunctionPtr(new Square());
 
-	double val = 0.5;
-	auto x = std::make_shared<Variable>(af::constant(val, 1));
-	auto a = (*A)(x);
-	auto b = (*B)(a);
-	auto y = (*C)(b);
+    double val = 0.5;
+    auto x     = std::make_shared<Variable>(af::constant(val, 1));
+    auto a     = (*A)(x);
+    auto b     = (*B)(a);
+    auto y     = (*C)(b);
 
-	double grad_val = 1.0;
-	y->grad = std::make_shared<NdArray>(af::constant(grad_val, 1));
-	y->Backward();
-    print(*x->grad); 
-	// std::cout << NdArrayPrinter(x->grad) << std::endl;
+    double grad_val = 1.0;
+    y->grad         = std::make_shared<NdArray>(af::constant(grad_val, 1));
+    y->Backward();
+    print(*x->grad);
+    // std::cout << NdArrayPrinter(x->grad) << std::endl;
 
     return 0;
 }
