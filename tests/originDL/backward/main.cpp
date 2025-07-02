8ab67427d302ef9abd5406f39d712d2bc116ea77
Jinbo <jinboom@foxmail.com>
2025-07-02 21:54:29 +0800

    test: set backend for test case dl_backward

--- tests/originDL/backward/main.cpp
+++ tests/originDL/backward/main.cpp
@@ -1,9 +1,87 @@
+#include <arrayfire.h>
+#include <getopt.h>
+#include <iostream>
 #include "originDL.h"
 
 using namespace dl;
 
-int main()
+void Usage()
 {
+    std::cout << "Usage: ./backend_demo -b [backend_code]\n"
+              << "Available backends:\n"
+              << "0 : CPU\n"
+              << "1 : CUDA\n"
+              << "2 : OpenCL\n";
+}
+
+int SetBackend(int argc, char **argv)
+{
+    int backend_code = 0;  // 默认CPU
+    int c;
+
+    // 解析命令行参数
+    while ((c = getopt(argc, argv, "b:h")) != -1)
+    {
+        switch (c)
+        {
+            case 'b':
+                backend_code = atoi(optarg);
+                break;
+            case 'h':
+                Usage();
+                return 0;
+            default:
+                return 1;
+        }
+    }
+
+    // 设置计算后端
+    try
+    {
+        switch (backend_code)
+        {
+            case 1:
+                af::setBackend(AF_BACKEND_CUDA);  // [4]() CUDA后端
+                break;
+            case 2:
+                af::setBackend(AF_BACKEND_OPENCL);  // OpenCL后端
+                break;
+            default:
+                af::setBackend(AF_BACKEND_CPU);  // CPU后端
+        }
+    }
+    catch (const af::exception &e)
+    {
+        loge("Failed to set backend: {}", e.what());
+        return 1;
+    }
+
+    // 验证后端设置
+    af::info();  // 输出设备信息
+    std::cout << std::endl;
+    switch (af::getActiveBackend())
+    {  // 获取当前后端
+        case AF_BACKEND_CUDA:
+            logw("Active Backend: CUDA");
+            break;
+        case AF_BACKEND_OPENCL:
+            logw("Active Backend: OpenCL");
+            break;
+        case AF_BACKEND_CPU:
+            logw("Active Backend: CPU");
+            break;
+        default:
+            loge("Invalid Backend");
+            exit(0);
+    }
+
+    return 0;
+}
+
+int main(int argc, char **argv)
+{
+    SetBackend(argc, argv);
+
     auto A = FunctionPtr(new Square());
     auto B = FunctionPtr(new Exp());
     auto C = FunctionPtr(new Square());
