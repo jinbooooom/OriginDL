f0da4ed1a9f7d0333df673a4c012aa61e1591e76
Jinbo <jinboom@foxmai.com>
2025-07-07 11:17:20 +0800

    feat(op): Add the subtraction operator.

---
+++ src/function/dlNeg.cpp
@@ -0,0 +1,44 @@
+#include "dlFunction.h"
+
+namespace dl
+{
+
+NdArrayPtrList Neg::Forward(const NdArrayPtrList &xs)
+{
+    auto outputs = NdArrayPtrList();
+    NdArray x0   = -(*xs[0]);
+    outputs.push_back(AsDLArrayPtr(x0));
+    return outputs;
+}
+
+NdArrayPtrList Neg::Backward(const NdArrayPtrList &gys)
+{
+    if (1 != gys.size())
+    {
+        logw("invalid argument size, not equal to 1");
+    }
+
+    auto gy  = -(*gys[0]);
+    auto gxs = NdArrayPtrList{AsDLArrayPtr(gy)};
+
+    return gxs;
+}
+
+VariablePtr neg(const VariablePtrList &xs)
+{
+    return (*std::shared_ptr<Function>(new Neg()))(xs)[0];
+}
+
+VariablePtr neg(const VariablePtr &x)
+{
+    auto xs = VariablePtrList();
+    xs.emplace_back(x);
+    return neg(xs);
+}
+
+VariablePtr operator-(const VariablePtr &x)
+{
+    return neg(x);
+}
+
+}  // namespace dl
